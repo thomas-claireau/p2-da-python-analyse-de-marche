e764077cb22d25ccf7bf30ddc3c39f5884854b6b
Thomas Claireau <thomas.claireau@gmail.com>
2020-12-23 16:12:38 +0100

    scrappy etape 1 -> availability OK

--- scrappy_etape_1.py
+++ scrappy_etape_1.py
@@ -3,14 +3,14 @@
 # récupérer :
 # product_page_url OK
 # universal_ product_code (upc) Ok
-# title
+# title OK
 # price_including_tax OK
 # price_excluding_tax OK
-# number_available OK
-# product_description
-# category
+# number_available
+# product_description OK
+# category OK
 # review_rating
-# image_url
+# image_url OK
 
 # Insérer les nouvelles données dans un CSV
 
@@ -24,15 +24,15 @@ class Product:
             setattr(self, information_name, information_value)
 
 
-product_informations = {"product_page_url": "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html", "universal_product_code": 0, "title": "", "price_including_tax": "£0",
-                        "price_excluding_tax": "£0", "number_available": 0, "product_description": "", "category": "", "review_rating": 0, "image_url": ""}
+product_informations = {
+    "product_page_url": "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"}
 
 response = requests.get(product_informations["product_page_url"])
 
 if response.ok:
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Récupérer universal_product_code / price_excluding_taxe / price_including_tax / number_available (tableau d'information en bas de page produit)
+    # Récupérer universal_product_code / price_excluding_taxe / price_including_tax (tableau d'information en bas de page produit)
     informations = soup.findAll("tr")
 
     for information in informations:
@@ -47,8 +47,6 @@ if response.ok:
             target_dict = "price_excluding_tax"
         if (information_label == "Price (incl. tax)"):
             target_dict = "price_including_tax"
-        if (information_label == "Availability"):
-            target_dict = "number_available"
 
         if "Â" in information_value:
             information_value = information_value.replace("Â", "")
@@ -74,5 +72,18 @@ if response.ok:
         'p').text
 
     # Récupérer review_rating (class star-rating + class indiquant le nombre d'étoile)
+
+    # Récupérer number_availability (instock outofstock en dessous du prix du produit)
+    availability = soup.select('p.availability.instock')
+
+    if availability:
+        availability = availability[0].text
+        availability = availability.replace('In stock (', '')
+        availability = availability.replace(' available)', '')
+        availability = int(availability)
+
+        product_informations["number_availability"] = availability
+    else:
+        product_informations["number_availability"] = 0
 
 # print(product_informations)
